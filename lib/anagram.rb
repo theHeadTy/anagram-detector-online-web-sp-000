9a80ed3aebb31577a6e8011bcfd90f511a1b6c87
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 00:44:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(string)
+    string.select { |str|
+      str.char.sort.join == word.split.sort.join
+    }
+  end
+end
